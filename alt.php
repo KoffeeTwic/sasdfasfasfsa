709ba1b0ac7d6851586b0615081cb41bb642a7a4
KoffeeTwic <55558129+KoffeeTwic@users.noreply.github.com>
2020-04-16 13:25:37 +0300

    s2ş
    sss

---
+++ alt.php
@@ -0,0 +1,16 @@
+<!-- Alt Başlar --> 
+<div id="alt">
+		<div style="width: 990px; margin-right: auto; margin-left: auto;">
+        <div style="float:left">
+            <a href="index.html"><img src="images/logo.png" alt="" style="height:30px; width:150px;" /></a>
+        </div>
+        <div style="float:left; margin-left: 160px; ">
+			<?php echo footer(); ?>
+        </div>
+    </div>
+	<div style="clear: both"></div>
+</div>
+<!-- Alt Biter --> 
+</body>
+<!-- Genel Biter --> 
+</html>
